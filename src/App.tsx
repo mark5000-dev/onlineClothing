6f77b04a92428262b38c52bde3d59077bbbc325e
markgathoni <mark.gathoni@outlook.com>
2025-11-22 14:23:09 +0300

    chore: Serve the mock_category data from a json file to mirror a fetch from an api

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,17 @@ import { Routes, Route } from "react-router-dom";
 import { useAppDispatch } from "./redux/hooks";
 import { setCategories } from "./redux/features/categoriesSlice";
 import { setProducts } from "./redux/features/productsSlice";
-import { type Product } from "./model";
+import type { Product, Category} from "./model";
 
 const Home = lazy(() => import('./pages/Home'));
 const About = lazy(() => import('./pages/About'));
 const SingleProduct = lazy(() => import('./pages/SingleProduct'));
 const Cart = lazy(() => import('./pages/Cart'));
-const Category = lazy(() => import('./pages/Categories'));
+const Categories = lazy(() => import('./pages/Categories'));
 const SingleCategory = lazy(() => import('./pages/SingleCategory'));
 const Products = lazy(() => import('./pages/Products'));
 const Profile = lazy(()=> import('./pages/Profile'));
 const Wishlist = lazy(() => import('./pages/Wishlist'));
-
-import { mockCategories as categories } from "./model";
 
 async function fetchProducts(): Promise<Product[]> {
   try {
@@ -33,17 +31,36 @@ async function fetchProducts(): Promise<Product[]> {
   }
 }
 
+async function fetchCategories(): Promise<Category[]> {
+  try {
+    const response = await fetch("./sample_data/mock_categories.json");
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+    const jsonData: Category[] = await response.json();
+    return jsonData;
+  } catch (error) {
+    console.error("Error fetching or parsing JSON: ", error);
+    return [];
+  }
+
+  
+}
 
 export default function App() {
   const dispatch = useAppDispatch();
+
   useEffect(() => {
   fetchProducts().then((jsonData) => {
     if (jsonData) dispatch(setProducts(jsonData));
+
+  fetchCategories().then((jsonData) => {
+    if (jsonData) dispatch(setCategories(jsonData));
+
+  })
   });
   }, [dispatch]);
 
 
-  dispatch(setCategories(categories));
   return (
     <div className="min-h-screen">
       <Header />
@@ -55,7 +72,7 @@ export default function App() {
             <Route path="/cart" element={<Cart />} />
             <Route path="/wishlist" element={<Wishlist />} />
             <Route path="/about" element={<About />} />
-            <Route path="/categories" element={<Category />} />
+            <Route path="/categories" element={<Categories />} />
             <Route path="/category/:slug" element={<SingleCategory />} />
             <Route path="/profile" element={<Profile />} />
           </Routes>
